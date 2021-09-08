f89e64283cae0619e6d93c00d00019b8c680f3c9
bmcalkins <66654789+bmcalkins@users.noreply.github.com>
2021-09-08 11:27:30 -0400

    integrated DB

--- cardProj/Controller/LoginViewController.swift
+++ cardProj/Controller/LoginViewController.swift
@@ -7,13 +7,20 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func signInButtonTapped(_ sender: Any) {
+    }
+    @IBAction func signUpButtonTapped(_ sender: Any) {
+    }
+    @IBAction func guestButtonTapped(_ sender: Any) {
+    }
+    
 
 }
 
