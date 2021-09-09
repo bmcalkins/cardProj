65faacd1169730c51c3c67aac1518116d669097f
Jason Hillegass <62678079+Jhillega@users.noreply.github.com>
2021-09-09 10:44:21 -0400

    added in state logic, structs and FlashCardManager class

---
+++ cardProj/ViewModel/ViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  ViewModel.swift
+//  cardProj
+//
+//  Created by jason hillegass on 9/9/21.
+//
+
+import Foundation
+
+final public class FlashCardManager
+{
+    fileprivate var userView: UserViewState = UnknownTypeUser()
+    
+    public func enterApp(userName: String, userEmail: String)
+    {
+        userView = UnknownTypeUser(context: self)
+        userView.validate(userName: userName, userEmail: userEmail)
+        
+    }
+    
+    public func cardButtonTapped() {
+        let userViewCopy = userView
+        userViewCopy.cardButtonTapped()
+    }
+    
+    public func accountButtonTapped() {
+        let userViewCopy = userView
+        userViewCopy.accountButtonTapped()
+    }
+    
+    public func createButtonTapped() {
+        let userViewCopy = userView
+        userViewCopy.createButtonTapped()
+    }
+}
