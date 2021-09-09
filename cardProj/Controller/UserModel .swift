a697c1c122da2d2769a5d46034ee19cdff7c0504
bmcalkins <66654789+bmcalkins@users.noreply.github.com>
2021-09-08 22:27:13 -0400

    worked on auth

---
+++ cardProj/Controller/UserModel .swift
@@ -0,0 +1,15 @@
+//
+//  UserModel .swift
+//  cardProj
+//
+//  Created by Bradley Calkins on 9/8/21.
+//
+
+import Foundation
+
+struct User: Codable {
+    var username: String
+    var email: String 
+    
+
+}
