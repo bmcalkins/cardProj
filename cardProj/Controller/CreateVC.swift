46d93f7e0837cd91586f7ef90bfb23f8bec83ec9
bmcalkins <66654789+bmcalkins@users.noreply.github.com>
2021-09-13 09:09:13 -0400

    Re attached SB

--- cardProj/Controller/CreateVC.swift
+++ cardProj/Controller/CreateVC.swift
@@ -11,9 +11,12 @@ class CreateVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
+        
         // Do any additional setup after loading the view.
+        
+     
     }
+    
     
 
     /*
